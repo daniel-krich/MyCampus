465e52eb560ad35224a1f3ae6d479eb802869442
zogov123 <danielkrich147@gmail.com>
2022-08-16 01:04:23 +0300

    my course meeting pagination

---
+++ MyCampusUI/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace MyCampusUI.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static string ToLongDateHebrewString(this DateTime date)
+    {
+        CultureInfo il = new CultureInfo("he-IL");
+        return date.ToString("f", il);
+    }
+}
