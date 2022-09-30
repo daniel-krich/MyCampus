4738a09c6d44dadc1bb01339b92192e4da7f0c1f
zogov123 <danielkrich147@gmail.com>
2022-09-30 16:56:09 +0300

    json data seed

---
+++ MyCampusData/Helpers/SeedHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyCampusData.Helpers
+{
+    public static class SeedHelper
+    {
+        public static List<TEntity> SeedData<TEntity>(string fileName)
+        {
+            string? currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if(currentDirectory == null) throw new DirectoryNotFoundException("Assembly directory not found");
+            string path = "seed";
+            string fullPath = Path.Combine(currentDirectory, path, fileName);
+
+            var result = new List<TEntity>();
+            using (StreamReader reader = new StreamReader(fullPath))
+            {
+                string json = reader.ReadToEnd();
+                result = JsonSerializer.Deserialize<List<TEntity>>(json);
+
+                if (result == null)
+                    throw new NullReferenceException("Error while parsing json file");
+            }
+
+            return result;
+        }
+    }
+}
