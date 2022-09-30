1b7e2c2a63678cc827165781853e70e9ce7e2878
zogov123 <danielkrich147@gmail.com>
2022-09-30 17:26:14 +0300

    nlog setup

--- MyCampusUI/Program.cs
+++ MyCampusUI/Program.cs
@@ -8,50 +8,71 @@ using MyCampusUI.Services;
 using MyCampusUI.Interfaces.Services;
 using MyCampusUI.Middleware;
 using Microsoft.AspNetCore.Authorization;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Info("Application init.");
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<IAuthenticationStateService, AuthenticationStateService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<ICustomNavigationService, CustomNavigationService>();
-builder.Services.AddScoped<IAssignmentManagerService, AssignmentManagerService>();
-builder.Services.AddScoped<IQuizManagerService, QuizManagerService>();
-builder.Services.AddScoped<ICourseManagerService, CourseManagerService>();
-builder.Services.AddDbContextFactory<CampusContext>();
-
-builder.Services.AddAuthentication(opt =>
+try
 {
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-   .AddJwtBearer();
-builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IBundleFilesService, BundleFilesService>();
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
+    builder.Host.UseNLog();
 
-builder.Services.AddAuthorization();
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddScoped<IAuthenticationStateService, AuthenticationStateService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+    builder.Services.AddScoped<ICustomNavigationService, CustomNavigationService>();
+    builder.Services.AddScoped<IAssignmentManagerService, AssignmentManagerService>();
+    builder.Services.AddScoped<IQuizManagerService, QuizManagerService>();
+    builder.Services.AddScoped<ICourseManagerService, CourseManagerService>();
+    builder.Services.AddDbContextFactory<CampusContext>();
 
-var app = builder.Build();
+    builder.Services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+       .AddJwtBearer();
+    builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+    builder.Services.AddSingleton<IBundleFilesService, BundleFilesService>();
+
+    builder.Services.AddAuthorization();
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    //app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseMiddleware<AppendHostMiddleware>();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
 }
-
-//app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseMiddleware<AppendHostMiddleware>();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+catch (Exception exception)
+{
+    logger.Error(exception, "Application setup error.");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
